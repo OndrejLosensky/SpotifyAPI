097f516e34bebec2eb5610ea476bbcfa27115950
Ondrej Losensky <losenskyondrej@gmail.com>
2023-11-20 21:33:20 +0100

    error

--- src/artistsID.js
+++ src/artistsID.js
@@ -1,137 +1,133 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const logo = document.getElementById('logo');
-  
-    // Add click event to the logo for refreshing the page
-    if (logo) {
+  const logo = document.getElementById('logo');
+
+  // Add click event to the logo for refreshing the page
+  if (logo) {
       logo.addEventListener('click', function () {
-        location.reload();
+          location.reload();
       });
-    }
-  
-    // Replace 'YOUR_CLIENT_ID' and 'YOUR_CLIENT_SECRET' with your actual values
-    const clientId = '293dcee6ecde4116bb53b61373415d40';
-    const clientSecret = 'd0f053c0b4ec4f25874cf95cb2a952b6';
-  
-    let accessToken = 'BQAhKQmDue83cBhkDoI5j0MGoni_KNwWsUSp6NYdmsPfiastdsswo9lANycTqnHRWeF7Ih7v3HoU2tE9NlxbabzwhomTx_AQkhWykraCbNcaHEeEEfmZR2Pbkts';
-  
-    const artistInput = document.getElementById('artist-input');
-    const searchButton = document.getElementById('search-button');
-    const defaultArtistDescription = document.getElementById('artist-description-text');
-  
-    // Add event listener for keypress on the input field
-    artistInput.addEventListener('keypress', function (event) {
+  }
+
+  // Replace 'YOUR_CLIENT_ID' and 'YOUR_CLIENT_SECRET' with your actual values
+  const clientId = '293dcee6ecde4116bb53b61373415d40';
+  const clientSecret = 'd0f053c0b4ec4f25874cf95cb2a952b6';
+
+  let accessToken = '';
+
+  const artistInput = document.getElementById('artist-input');
+  const searchButton = document.getElementById('search-button');
+  const defaultArtistDescription = document.getElementById('artist-description-text');
+
+  // Add event listener for keypress on the input field
+  artistInput.addEventListener('keypress', function (event) {
       if (event.key === 'Enter') {
-        performSearch();
+          performSearch();
       }
-    });
-  
-    // Add event listener for click on the search button
-    searchButton.addEventListener('click', function () {
+  });
+
+  // Add event listener for click on the search button
+  searchButton.addEventListener('click', function () {
       performSearch();
-    });
-  
-    // Function to perform the search
-    function performSearch() {
-      const artistName = artistInput.value.trim();
-  
+  });
+
+  // Function to perform the search
+  function performSearch() {
+    const artistName = artistInput.value.trim();
+    console.log('Encoded artist name:', encodeURIComponent(artistName));
+
       if (artistName) {
-        // Hide the default artist description
-        if (defaultArtistDescription) {
-          defaultArtistDescription.style.display = 'none';
-        }
-  
-        // Call the function to fetch top tracks for the searched artist
-        fetchTopTracksForArtistByName(artistName);
-      } else {
-        console.log('Please enter an artist name.');
-        //alert('Please enter an artist name.');
-      }
-    }
-  
-    // Function to fetch top tracks for a specific artist by name
-    function fetchTopTracksForArtistByName(artistName) {
-      fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(artistName)}&type=artist&limit=1`, {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`
-        }
-      })
-        .then(response => response.json())
-        .then(data => {
-          if (data.artists && data.artists.items && data.artists.items.length > 0) {
-            const artistId = data.artists.items[0].id;
-            // Call the function to fetch top tracks for the found artist
-            fetchTopTracksForArtist(artistId);
-          } else {
-            console.error(`No artist found for the query: ${artistName}`);
-            //alert(`No artist found for the query: ${artistName}`)
+          // Hide the default artist description
+          if (defaultArtistDescription) {
+              defaultArtistDescription.style.display = 'none';
           }
-        })
-        .catch(error => {
-          console.error(`Error searching for artist ${artistName}:`, error);
-          //alert(`Error searching for artist ${artistName}`)
-        });
-    }
-  
-    // Function to refresh the access token
-    function refreshAccessToken() {
-      fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          "Authorization": `Basic ${btoa(`${clientId}:${clientSecret}`)}`
-        },
-        body: "grant_type=client_credentials&scope=user-top-read"
+
+          // Call the function to fetch top tracks for the searched artist
+          fetchTopTracksForArtist(artistName);
+      } else {
+          console.log('Please enter an artist name.');
+      }
+  }
+
+  // Function to fetch top tracks for a specific artist by name
+  function fetchTopTracksForArtist(artistName) {
+      console.log('Fetching top tracks for artist:', artistName);
+      fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(artistName)}&type=artist&limit=1`, {
+          headers: {
+              'Authorization': `Bearer ${accessToken}`
+          }
       })
-        .then(response => response.json())
-        .then(data => {
-          accessToken = data.access_token;
-          console.log("Access token refreshed:", accessToken);
-        })
-        .catch(error => {
-          console.error("Error refreshing access token:", error);
-        });
-    }
-  
-    // Function to fetch top tracks for a specific artist
-    function fetchTopTracksForArtist(artistId) {
-      fetch(`https://api.spotify.com/v1/artists/${artistId}/top-tracks?country=CZ`, {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`
-        }
-      })
-        .then(response => response.json())
-        .then(data => {
-          const topTracks = data.tracks;
-          // Handle the data and update the HTML
-          displayTracks(topTracks);
-        })
-        .catch(error => {
-          console.error(`Error fetching top tracks for artist ${artistId}:`, error);
-        });
-    }
-  
+          .then(response => response.json())
+          .then(data => {
+              console.log('Search API response:', data);
+              // Inside the fetchTopTracksForArtistByName function
+              if (data.artists && data.artists.items && data.artists.items.length > 0) {
+                const artistId = data.artists.items[0].id;
+                // Call the function to fetch top tracks for the found artist
+                fetchTopTracksForArtist(artistId);
+              } else {
+                console.error(`No artist found for the query: ${artistName}`);
+                // You can display a message to the user or handle it in another way
+              }
+          })
+          .catch(error => {
+              console.error(`Error searching for artist ${artistName}:`, error);
+          });
+  }
+
     // Initial request for top tracks for a specific artist
-    // Replace 'ARTIST_ID' with the actual artist ID you want to fetch tracks for
-    const artistId = ''; // 5iYtjtFv6SvHX95ny4fVEz //3FCo1sUkVbwKnO2m0Z7Bp9
-    fetchTopTracksForArtist(artistId);
-  
-    // Set up token refresh every 30 minutes
-    setInterval(refreshAccessToken, 30 * 60 * 1000);
+  const artistId = ''; // Remove the hardcoded artist ID
+  fetchTopTracksForArtist(artistId);
+  // Function to refresh the access token
+  function refreshAccessToken() {
+      fetch("https://accounts.spotify.com/api/token", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              "Authorization": `Basic ${btoa(`${clientId}:${clientSecret}`)}`
+          },
+          body: "grant_type=client_credentials&scope=user-top-read"
+      })
+          .then(response => response.json())
+          .then(data => {
+              accessToken = data.access_token;
+              console.log("Access token refreshed:", accessToken);
+          })
+          .catch(error => {
+              console.error("Error refreshing access token:", error);
+          });
+  }
+
+  // Call refreshAccessToken to fetch an initial token
+  refreshAccessToken();
+
+  // Set up token refresh every 30 minutes
+  setInterval(refreshAccessToken, 30 * 60 * 1000);
   
     function displayTracks(tracks) {
+      console.log('Displaying tracks:', tracks);
       const tracksContainer = document.getElementById('tracks-container');
       const chosenArtistHeading = document.getElementById('chosen-artist');
   
       // Check if the container element is found
-      if (!tracksContainer) {
-        console.error('Error: Tracks container not found.');
-        return;
-      }
-  
-      // Clear existing content
-      tracksContainer.innerHTML = '';
-  
-      chosenArtistHeading.textContent = `${tracks[0].artists[0].name}`;
+        if (!tracksContainer) {
+          console.error('Error: Tracks container not found.');
+          return;
+        }
+
+        // Clear existing content
+        tracksContainer.innerHTML = '';
+
+        if (tracks.length > 0 && tracks[0].artists && tracks[0].artists.length > 0) {
+          chosenArtistHeading.textContent = `${tracks[0].artists[0].name}`;
+
+          // Loop through each track and display its information
+          tracks.forEach(track => {
+            // ... (existing code)
+          });
+        } else {
+          chosenArtistHeading.textContent = 'No artist found';
+          // You can display a message to the user or handle it in another way
+        }
   
       // Loop through each track and display its information
       tracks.forEach(track => {
